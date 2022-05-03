659f922f4c54a8eec76aa970f455fe5e12de9e4f
MostafaT-soli <103927426+MostafaT-soli@users.noreply.github.com>
2022-05-03 11:09:32 +0200

    Create VAR.py
    all variables that can be added here

---
+++ VAR.py
@@ -0,0 +1,11 @@
+TOKEN = 'OTUxODAxNjk3MTY3MDk3ODc2.YiswaA.N2Lxf3ExChRs2olVeagAbtrNI88' # token to run bot
+
+IDS = ['0x942812150CEBD40Ee00AC0aEC151Ed460F4d976c'] # wallet Ids in list
+
+GUILD_ID = 956926677445926942                                                                     # Guild ID
+
+TIME = 3600                                                                                         # time in se
+
+CHANNEL_ID= 956927664751530034
+
+GUILD = 'PEGATO'                                                               # channel ID
